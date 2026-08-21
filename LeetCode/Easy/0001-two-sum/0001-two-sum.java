a29ebbb66223e8659675caad8cdce48703683eab
Manasdeep Behura <157208591+Tattvamasiii@users.noreply.github.com>
2026-08-21 13:22:27 +0530

    2 ms (99.34%) 47.3 MB (11.23%) 08-21-2026 0001-two-sum Easy .java

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,36 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-     int n = nums.length;
-    int[][] pairs = new int[n][2]; // each row: {value, originalIndex}
-    for (int i = 0; i < n; i++) {
-        pairs[i][0] = nums[i];
-        pairs[i][1] = i;
+       HashMap<Integer, Integer> res= new HashMap<>();
+
+         for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+
+        if (res.containsKey(complement)) {
+            return new int[]{res.get(complement), i};
+        }
+
+        res.put(nums[i], i);
     }
 
-    // sort pairs by value (column 0), using insertion sort adapted for pairs
-    for (int i = 0; i < pairs.length - 1; i++) {
-        for (int j = i + 1; j > 0; j--) {
-            if (pairs[j][0] < pairs[j - 1][0]) {
-                int[] temp = pairs[j];
-                pairs[j] = pairs[j - 1];
-                pairs[j - 1] = temp;
-            } else {
-                break;
-            }
-        }
-    }
-
-    int left = 0, right = n - 1;
-    while (left < right) {
-        int sum = pairs[left][0] + pairs[right][0];
-        if (sum == target) {
-            return new int[]{pairs[left][1], pairs[right][1]};
-        } else if (sum > target) {
-            right--;
-        } else {
-            left++;
-        }
-    }
-    return new int[]{-1, -1};
-    }
+    return new int[]{-1, -1}; // no solution found
 }
+    }

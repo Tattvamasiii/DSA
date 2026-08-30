df3e784fd0fa8486f043604f883fc01f99b29be8
Manasdeep Behura <157208591+Tattvamasiii@users.noreply.github.com>
2026-08-30 11:47:20 +0530

    0 ms (100%) 42.2 MB (36.02%) 08-30-2026 0070-climbing-stairs Easy .java

---
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int[] memo= new int[46];
+    public int climbStairs(int n) {
+     if(n==1) return 1;
+     if(n==2) return 2;
+     if(memo[n]!=0){
+         return memo[n];
+     }
+     int result= climbStairs(n-1) + climbStairs(n-2);
+      memo[n]=result;
+     return result;
+    }
+}

157fba4b786035efffac2f2bc03ededfe4a2c6b9
Manasdeep Behura <157208591+Tattvamasiii@users.noreply.github.com>
2026-09-04 08:29:37 +0530

    2 ms (34.7%) 46.2 MB (7.51%) 09-04-2026 0046-permutations Medium .java

---
+++ LeetCode/Medium/0046-permutations/0046-permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+              List<List<Integer>> list = new ArrayList<>();
+        SubPermu(new ArrayList<>(), nums, 0, list);
+        return list;
+    }
+
+    static void SubPermu(ArrayList<Integer> p, int[] nums, int index, List<List<Integer>> list) {
+        if (index == nums.length) {
+            list.add(new ArrayList<>(p));
+            return;
+        }
+        int ch = nums[index];
+        ArrayList<Integer> ans = new ArrayList<>(); // Save it in answer whatever indiviual recusion call is getting
+        for (int i = 0; i <= p.size(); i++) {
+            List<Integer>  f = p.subList(0, i);
+            List<Integer>  s = p.subList(i, p.size());
+
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.addAll(f);
+            temp.add(ch);
+            temp.addAll(s);
+            SubPermu(temp, nums, index + 1, list );
+    }
+}
+}
